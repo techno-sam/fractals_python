f262b2a39289bb2fd31a63974ff0ce50fe606840
techno-sam <77073745+techno-sam@users.noreply.github.com>
2023-10-03 16:24:45 -0700

    fix histogram

--- colorization_test_provider.py
+++ colorization_test_provider.py
@@ -24,7 +24,6 @@ def draw_averaging(dim: int, pygame, screen, y_x: list[list[tuple[int, float, fl
 
 def draw(dim: int, pygame, screen, y_x: list[list[tuple[int, float, float]]], display_update: bool = False):
     counts = {}
-    max_c = 0
     for y in range(dim):
         for x in range(dim):
             it, _, _ = y_x[y][x]
@@ -32,14 +31,16 @@ def draw(dim: int, pygame, screen, y_x: list[list[tuple[int, float, float]]], di
                 if it not in counts:
                     counts[it] = 0
                 counts[it] += 1
-                max_c = max(max_c, counts[it])
+    total = 0
+    for count in counts.values():
+        total += count
 
     for y in range(dim):
-        if display_update:
+        if display_update and y%10==0:
             pygame.display.update()
         for x in range(dim):
             data = y_x[y][x]
-            screen.set_at((x, y), color_counts(*data, x, y, counts, max_c))
+            screen.set_at((x, y), color_counts(*data, x, y, counts, total))
     if display_update:
         pygame.display.update()
 
@@ -66,14 +67,18 @@ def color(iters: int, fx: float, fy: float, x: int, y: int, avg_iters: float) ->
         return (255, 0, 0)
 
 
-def color_counts(iters: int, fx: float, fy: float, x: int, y: int, counts: dict[int, int], max_c: int) -> tuple[int, int, int]:
-    PAL_SIZE = max_c * 0.7
+def color_counts(iters: int, fx: float, fy: float, x: int, y: int, counts: dict[int, int], total_count: int) -> tuple[int, int, int]:
     if iters == -1:
         return 0, 0, 0
 
-    histogram_count = counts[iters]
+    hue = 0
+    for i in range(iters):
+        if i in counts:
+            hue += counts[i] / total_count
+    #if True:
+    #    return _clean(_to_255(colorsys.hls_to_rgb( (((iters/1000)*360)**1.5)/360 , 0.5, 1)))
     try:
-        return _clean(_to_255(colorsys.hls_to_rgb((histogram_count % PAL_SIZE) / PAL_SIZE, 0.5, 1)))
+        return _clean(_to_255(colorsys.hls_to_rgb(hue**1.5, 0.5, 1)))
     except ValueError:
         return 255, 0, 0
 
